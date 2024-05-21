f9fe59a40911471211e9752cd816c63a3ef956a5
MIFDC <e1221840@u.nus.edu>
2024-05-21 16:44:50 +0800

    camera and hitresult completed

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import {
   ViroARScene,
   ViroARSceneNavigator,
+  ViroAnimations,
   ViroText,
   ViroBox,
   ViroSphere,
@@ -13,8 +14,8 @@ import {
   ViroTrackingStateConstants,
 } from "@viro-community/react-viro";
 import React, { useState } from "react";
-import { StyleSheet, View, Text, Touchable} from "react-native";
-import { setConstantValue } from "typescript";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { transform } from "typescript";
 
 
 const InitialScene = () => {
@@ -24,29 +25,48 @@ const InitialScene = () => {
       diffuseTexture: require('./assets/Texture/Wood.jpg')
     }
   })
+  ViroAnimations.registerAnimations({
+    loopRotate: {
+      dureation: 1000,
+      properties: {
+        rotateX: '+=45'
+      }
+    }
+  })
+
+  const [currentOrientation, setCurrentOrientation] = useState(null);
+  const [currentHitResults, setCurrentHitResults] = useState(null);
+  const [currentData, setCurrentData] = useState(null);
+
+
+  function onCameraARHitHandler(transform) {
+    const cameraOrientationValue = transform['cameraOrientation'];
+    const hitTestResults = transform['hitTestResults'];
+
+    console.log("cameraOrientationValue",cameraOrientationValue);
+    console.log("hitTestResults", hitTestResults);
+    
+  }
 
   return (
-    <ViroARScene>
+    <ViroARScene
+      onCameraARHitTest={(transformInfo) => onCameraARHitHandler(transformInfo)}>
       {/*<ViroText
         text={"Hello World"}
         position={[0, 0, -1]}
         style={styles.helloWorldTextStyle} />*/}
-      <ViroCamera active={true} />
+      <ViroCamera position={[0, 0, 0]} active={true} />
       <ViroBox
         scale={[0.2, 0.2, 0.2]}
-        position={[0, 0, -1]}
-        materials={["wood"]} />
+        position={[0, 0.5, -1]}
+        materials={["wood"]}
+      /*animation={{ name: 'loopRotate', loop: true, run: true }}*/
+      />
     </ViroARScene>
   )
 }
 
 export default () => {
-  const [currentPosition, setCurrentPosition] = useState([0, 0, 0]);
-
-  const getPosition = () => {
-    setCurrentPosition(ViroOrbitCamera.position)
-    console.log(currentPosition);
-  }
 
   return (
     <View style={styles.mainView}>
@@ -56,9 +76,9 @@ export default () => {
         }}
         style={{ flex: 1 }} />
       <View style={styles.controlView}>
-        <Touchable onPress={() => getPosition()}>
+        <TouchableOpacity>
           <Text>Get the position</Text>
-        </Touchable>
+        </TouchableOpacity>
       </View>
     </View>
 
